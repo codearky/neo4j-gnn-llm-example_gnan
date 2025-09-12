1eae8a9f17963b74a08c85f02036e98193ea0f36
yoavk <yoav.korsade@mobileye.com>
2025-09-12 20:04:56 +0000

    add run.sh

---
+++ data-loading/run.sh
@@ -0,0 +1,12 @@
+wget -O - https://debian.neo4j.com/neotechnology.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/neotechnology.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/neotechnology.gpg] https://debian.neo4j.com stable latest' | sudo tee -a /etc/apt/sources.list.d/neo4j.list
+sudo apt-get update
+sudo apt-get install -y neo4j=1:2025.07.0
+cp /var/lib/neo4j/products/* /var/lib/neo4j/plugins/
+echo 'dbms.security.procedures.allowlist=gds.*' >> /etc/neo4j/neo4j.conf
+NEO4J_PASSWORD=${NEO4J_PASSWORD:-test12345}
+neo4j stop || true
+neo4j-admin dbms set-initial-password "$NEO4J_PASSWORD" || true
+neo4j start
+curl -sS -u neo4j:"$NEO4J_PASSWORD" -H 'Content-Type: application/json' -d '{"statements":[{"statement":"RETURN 1 AS test"}]}' http://localhost:7474/db/neo4j/tx/commit
+# /home/me.docker/.conda/envs/pyg_new/bin/python /worxpace/neo4j-gnn-llm-example/data-loading/load_data.py
