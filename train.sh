ef5f64ef52570324ff5bbcd92e6875cf5135b45c
yoavk <yoav.korsade@mobileye.com>
2025-09-12 21:53:35 +0000

    add train sh

---
+++ train.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+CONDA_DEFAULT_ENV=pyg_new NEO4J_URI=bolt://localhost:7687 NEO4J_USERNAME=neo4j NEO4J_PASSWORD=password python train.py --checkpointing --llama_version llama3.1-8b --retrieval_config_version 0 --g_retriever_config_version 0 --eval_batch_size 4 --num_gnn_layers 4 --algo_config_version 1
