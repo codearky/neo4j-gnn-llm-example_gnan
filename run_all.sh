b645ad70ea4571424808b54bb88e7fbc0bfd5cc7
yoavk <yoav.korsade@mobileye.com>
2025-11-15 07:53:52 +0000

    Update training script parameters for improved performance and context handling. Adjust evaluation batch size to 1, increment algorithm configuration version, and include additional context options for graph processing.

--- run_all.sh
+++ run_all.sh
@@ -13,5 +13,7 @@ cd data-loading
 # python emb_download.py
 python load_data.py
 cd ..
-NEO4J_URI=bolt://localhost:7687 NEO4J_USERNAME=neo4j NEO4J_PASSWORD=test12345 python train.py --checkpointing --llama_version llama3.1-8b --retrieval_config_version 0 --g_retriever_config_version 0 --eval_batch_size 4 --num_gnn_layers 4 --algo_config_version 2
+NEO4J_URI=bolt://localhost:7687 NEO4J_USERNAME=neo4j NEO4J_PASSWORD=test12345 python train.py --checkpointing \
+--llama_version llama3.1-8b --retrieval_config_version 0 --g_retriever_config_version 0 --eval_batch_size 1 \
+--num_gnn_layers 4 --algo_config_version 3 --include_pcst_desc_context --topk_gnan_nodes_context 0
 
